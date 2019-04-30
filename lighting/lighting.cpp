d6798629b7bba663b9c5af280c5b4e456f2f5d08
Cole Knapp <knappcole64@gmail.com>
2019-04-30 10:33:34 -0400

    continued lighting color tutorial.

--- lighting/lighting.cpp
+++ lighting/lighting.cpp
@@ -1,5 +1,63 @@
-/***********************
-Move camera and shader code to the include directory
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-************************/
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+#include "shader.hpp"
+#include "camera.hpp"
+
+#include <iostream>
+
+// Callback functions
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffet, double yoffset);
+
+void process_input(GLFWwindow* window);
+
+// Settings 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// Timing 
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// Lighting 
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+int main() {
+	// glfw init and config
+	// --------------------
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	// glfw window creation
+	// --------------------
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Lighting", NULL, NULL);
+	if (!window) {
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+
+}
